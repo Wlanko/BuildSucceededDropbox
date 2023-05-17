fedc7c84da4f820b6e5a7a1443e7afc15daff2a8
Vlad Kyrylenko <sorvigolovaqwerty@gmail.com>
2023-05-17 19:20:09 +0300

    Initial Commit

---
+++ BuildSucceededDropbox/BuildSucceededDropboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildSucceededDropboxApp.swift
+//  BuildSucceededDropbox
+//
+//  Created by Vlad Kyrylenko on 17.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BuildSucceededDropboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
